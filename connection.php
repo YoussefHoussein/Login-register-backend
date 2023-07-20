cb4a7ed222115ce2589745d7bec330e68ac32b44
unknown <youssefhoussein701@gmail.com>
2023-07-21 01:37:57 +0300

    <backend>: modify the connection script

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: *');
+    header('Content-Type: application/json; charset=UTF-8');
+    header('Access-Control-Max-Age: 3600');
+
+    $host = "localhost";
+    $db_user = "root";
+    $db_pass = null;
+    $db_name = "login_db";
+
+    $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+    if (!$mysqli) {
+    die('a connection was unsuccesful');
+    }
